08d0e3062d35f39eafd3507fe659521ea66aa69f
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:01:17 +0800

    calculate length of prefix

--- remove_prefix.py
+++ remove_prefix.py
@@ -2,7 +2,9 @@
 user_input = input("Enter a string: ")
 prefix = input("Enter the prefix to remove: ")
 
-# calculate length of prefixx
+# calculate length of prefix
+prefix_length = len(prefix)
+
 # check if beginning of string matches pefis
 #     if yes, remove, else, keep
 # print result
