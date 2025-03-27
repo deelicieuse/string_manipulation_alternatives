7159175b58b82d4ca5cedb34af1b822c5c2b9068
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:02:40 +0800

    input string and width

--- manual_center.py
+++ manual_center.py
@@ -1,4 +1,7 @@
 # prompt string and width
+user_input = input("Enter a string: ")
+desired_width = int(input("Enter width: "))
+
 # if length of string < width, calculater space and split string into left and right
 # else keep string as is
 # print result
