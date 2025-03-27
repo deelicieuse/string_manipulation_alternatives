c0b160078ceecdf87b286a6b25765b43f650ac9e
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:28:20 +0800

    if a letter is found, set to true, if lowercase, false

--- manual_isupper.py
+++ manual_isupper.py
@@ -5,5 +5,12 @@ found_letter = False
 is_all_upper = True
 
 # loop character
+for character in user_input:
 #     if a letter is found, set to true, if lowercase, false
+    if ("A" <= character <= "Z") or ("a" <= character <= "z"):
+        found_letter = True
+        if "a" <= character <= "z":
+            is_all_upper = False
+            break
+
 # if one letter is found and no lowercase, print true, else false
