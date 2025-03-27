7a5b9ff57a895a9852545bb465cce39b548eb768
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:53:40 +0800

    input string and witdth

--- manual_ljust.py
+++ manual_ljust.py
@@ -1,4 +1,7 @@
 # prompt string and width
+user_input = input("Enter a string: ")
+desired_width = int(input("Enter width: "))
+
 # if length of string is less than width, calculate spaces
 # else keep string
 # print result
