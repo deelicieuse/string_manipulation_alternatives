9f0caa7e54425327c8511c06396502069be50311
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:08:26 +0800

    if lowercase, convert to uppercase

--- manual_swapcase.py
+++ manual_swapcase.py
@@ -3,8 +3,9 @@ user_input = input("Enter a string: ")
 result = "" # initialize empty string
 
 # for each character,
-
-#     if lowercase, convert to uppercase
+for character in user_input:
+    if "a" <= character <= "z": #     if lowercase, convert to uppercase
+        result += chr(ord(character) - 32)
 #     if uppercase, convert to lowercase
 #     else, keep string as is
 # print result
