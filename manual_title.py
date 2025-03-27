83db7272be9d87592aa2a4d0bd0ba58746467e5e
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:38:36 +0800

    read each word, get 1st caharacter and remains

--- manual_title.py
+++ manual_title.py
@@ -4,6 +4,10 @@ words = user_input.split() # split string into words
 result = ""
 
 # for each word
+for word in words:
+    if word:
+        first_character = word[0]
+        rest_of_word = word[1:]
 #     capitalize first character
 #     convert remaiing character to lower case
 # combine and print result
