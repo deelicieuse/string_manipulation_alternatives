207628753d693f8b0daa9c544543ded45508e419
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:54:26 +0800

    if length of string is less than width, calculate spaces

--- manual_ljust.py
+++ manual_ljust.py
@@ -3,5 +3,8 @@ user_input = input("Enter a string: ")
 desired_width = int(input("Enter width: "))
 
 # if length of string is less than width, calculate spaces
+if len(user_input) < desired_width:
+    result = user_input + " " * (desired_width - len(user_input))
+
 # else keep string
 # print result
