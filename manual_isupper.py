e2b63677f5b3a0cca154eabba8f66be375a2c6dc
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:27:15 +0800

    intialize  to false

--- manual_isupper.py
+++ manual_isupper.py
@@ -1,6 +1,9 @@
 # input string
 user_input = input("Enter a string: ")
-# intialize variable to false
+# intialize  to false
+found_letter = False
+is_all_upper = True
+
 # loop character
 #     if a letter is found, set to true, if lowercase, false
 # if one letter is found and no lowercase, print true, else false
