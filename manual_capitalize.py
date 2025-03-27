0bf3735009e53dce466374a9438626e43ba59dd2
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:20:44 +0800

    prompt for string

--- manual_capitalize.py
+++ manual_capitalize.py
@@ -1,4 +1,6 @@
 # prompt for string
+user_input = input("Enter a string: ")
+
 # if string is not empty, capitalize first character and convert remaining characters to lower case
 # else, set as empty string
 # print result
