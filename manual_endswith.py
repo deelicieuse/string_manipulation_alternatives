ca39edb3afed63e23c69419f2eecb77d018de389
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:35:59 +0800

    input string and suffix

---
+++ manual_endswith.py
@@ -0,0 +1,8 @@
+# prompt string and suffix
+user_input = input("Enter a string: ")
+suffix = input("Enter the suffix to check: ")
+
+# calculate length of suffix
+# if suffix is longer than string, print false
+#     else compare end of string with suffix
+# print true if they match, else false
