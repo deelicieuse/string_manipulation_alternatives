ca2cc64004ad36d1866827559a8e79cb75f5cd2c
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:23:18 +0800

    # if string is not empty, capitalize first character

--- manual_capitalize.py
+++ manual_capitalize.py
@@ -1,6 +1,13 @@
 # prompt for string
 user_input = input("Enter a string: ")
 
-# if string is not empty, capitalize first character and convert remaining characters to lower case
+# if string is not empty, capitalize first character
+if user_input:
+    first_character = user_input[0]
+    rest_of_string = user_input[1:]
+    if "a" <= first_character <= "z":
+        first_character = chr(ord(first_character) - 32)
+
+# and convert remaining characters to lower case
 # else, set as empty string
 # print result
