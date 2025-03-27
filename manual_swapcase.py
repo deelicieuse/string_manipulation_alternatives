dbc2bea0836cba80defcd74e88b4e30768cb014e
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:07:45 +0800

    initialize empty string

--- manual_swapcase.py
+++ manual_swapcase.py
@@ -1,8 +1,9 @@
 # prompt for string
 user_input = input("Enter a string: ")
+result = "" # initialize empty string
 
-# initialize empth string
 # for each character,
+
 #     if lowercase, convert to uppercase
 #     if uppercase, convert to lowercase
 #     else, keep string as is
