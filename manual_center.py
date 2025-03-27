8193700dbd26dbb7fb809db651518be4b832587d
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:03:32 +0800

    if length of string < width, calculater space

--- manual_center.py
+++ manual_center.py
@@ -2,6 +2,9 @@
 user_input = input("Enter a string: ")
 desired_width = int(input("Enter width: "))
 
-# if length of string < width, calculater space and split string into left and right
+# if length of string < width, calculater space
+if len(user_input) < desired_width:
+    total_padding = desired_width - len(user_input)
+# and split string into left and right
 # else keep string as is
 # print result
