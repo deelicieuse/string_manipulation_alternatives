25dda64d8852a58173e3ce82c9d662d6589ad0cc
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 18:06:52 +0800

    pseudocode

---
+++ manual_swapcase.py
@@ -0,0 +1,7 @@
+# prompt for string
+# initialize empth string
+# for each character,
+#     if lowercase, convert to uppercase
+#     if uppercase, convert to lowercase
+#     else, keep string as is
+# print result
