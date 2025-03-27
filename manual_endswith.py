ef2700564c8532ada16290b49503395ccf5488a9
deelicieuse <libatiqueangelique@gmail.com>
2025-03-27 17:36:16 +0800

    calculate length of suffix

--- manual_endswith.py
+++ manual_endswith.py
@@ -3,6 +3,7 @@ user_input = input("Enter a string: ")
 suffix = input("Enter the suffix to check: ")
 
 # calculate length of suffix
+suffix_length = len(suffix)
 # if suffix is longer than string, print false
 #     else compare end of string with suffix
 # print true if they match, else false
