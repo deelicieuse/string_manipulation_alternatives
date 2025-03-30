8efcbaaf50a5f63650ab60ca55c8eae5d0756a27
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:40:49 +0800

    input string

--- manual_rstrip.py
+++ manual_rstrip.py
@@ -1,4 +1,6 @@
 # prompt for string
+user_input = input("Enter a string: ")
+
 # set to last character of the string
 # while non-negative and character at index is a space, decrement index
 # slice the string from start and store in result
