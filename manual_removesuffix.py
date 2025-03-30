0bdbf42b85662844630ccc5f961e267783fe3be2
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:45:55 +0800

    input string and suffix

--- manual_removesuffix.py
+++ manual_removesuffix.py
@@ -1,4 +1,6 @@
 # prompt for string and suffix
+user_input = input("Enter a string: ")
+
 # calculate length of suffix
 # if the substring at the end matches suffix, remove it
 # else, keep the string
