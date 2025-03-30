6e467cd6b5d7d59996ec5924d35404e750e693bb
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:40:11 +0800

    pseudocode

---
+++ manual_rstrip.py
@@ -0,0 +1,5 @@
+# prompt for string
+# set to last character of the string
+# while non-negative and character at index is a space, decrement index
+# slice the string from start and store in result
+# print result
