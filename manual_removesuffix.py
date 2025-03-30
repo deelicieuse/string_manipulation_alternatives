1dba9900e028fc27f03042d836631e4a60916191
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:47:10 +0800

    if the substring at the end matches suffix, remove it

--- manual_removesuffix.py
+++ manual_removesuffix.py
@@ -3,5 +3,8 @@ user_input = input("Enter a string: ")
 suffix = input("Enter suffix to remove: ")
 suffix_len = len(suffix) # calculate length of suffix
 # if the substring at the end matches suffix, remove it
+if suffix_len <= len(user_input) and user_input[-suffix_len:] == suffix:
+    result = user_input[:-suffix_len]
+
 # else, keep the string
 # print result
