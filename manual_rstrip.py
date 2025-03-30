19254a7088d9334982fd8784364652cc598f891e
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:42:07 +0800

    while non-negative and character at index is a space, decrement index

--- manual_rstrip.py
+++ manual_rstrip.py
@@ -1,9 +1,10 @@
 # prompt for string
 user_input = input("Enter a string: ")
-
-# set to last character of the string
-index = len(user_input) - 1
+index = len(user_input) - 1 # set to last character of the string
 
 # while non-negative and character at index is a space, decrement index
+while index >= 0 and user_input[index] == " ":
+    index -= 1
+
 # slice the string from start and store in result
 # print result
