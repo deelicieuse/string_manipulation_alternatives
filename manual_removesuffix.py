a9404506f2339b55663bcee2e9f508ae71b2fb5b
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:46:30 +0800

    calculate length of suffix

--- manual_removesuffix.py
+++ manual_removesuffix.py
@@ -1,7 +1,7 @@
 # prompt for string and suffix
 user_input = input("Enter a string: ")
-
-# calculate length of suffix
+suffix = input("Enter suffix to remove: ")
+suffix_len = len(suffix) # calculate length of suffix
 # if the substring at the end matches suffix, remove it
 # else, keep the string
 # print result
