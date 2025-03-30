9954e5812344de183fb76860331b25b85f5326cb
deelicieuse <libatiqueangelique@gmail.com>
2025-03-30 23:41:22 +0800

    set to last character of the string

--- manual_rstrip.py
+++ manual_rstrip.py
@@ -2,6 +2,8 @@
 user_input = input("Enter a string: ")
 
 # set to last character of the string
+index = len(user_input) - 1
+
 # while non-negative and character at index is a space, decrement index
 # slice the string from start and store in result
 # print result
