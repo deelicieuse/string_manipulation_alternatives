badae5c4e5979c23364ad85ad4e371812828a4a6
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:33:21 +0800

    initialize to zero

--- manual_count.py
+++ manual_count.py
@@ -1,6 +1,6 @@
 user_input = input("Enter a string: ")# prompt for string and substring to count
 sub_str = input("Enter substring to count: ")
-# initialize count to 0
+count = 0 # initialize count to 0
 # loop over each start of string, check if substring matches
 # add count if match found
 # print count
