bbecf89142a5edf75414d0071237aceae6d402b6
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:45:58 +0800

    if not, print "Not Found"

--- manual_rindex.py
+++ manual_rindex.py
@@ -2,7 +2,7 @@ user_input = input("Enter a string: ") # prompt for string and substring
 sub_str = input("Enter substring to find from the right: ")
 sub_len = len(sub_str)
 found = False
-for num in range(len(user_input) - sub_len, -1, -1): # loop over each index start in the string, check for a match
+for num in range(len(user_input) - sub_len, -1, -1): # loop backwards over each index start in the string, check for a match
     if user_input[num:num + sub_len] == sub_str: # if  match found, print index and break
         print(f"Result: {num}")
         found = True
