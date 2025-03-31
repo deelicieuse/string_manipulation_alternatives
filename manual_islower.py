09e04d1e52cc771115fa65a06c84b25a05778951
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:07:22 +0800

    print result

--- manual_islower.py
+++ manual_islower.py
@@ -7,4 +7,4 @@ for char in user_input: # for each character,
         if not ("a" <= char <= "z"): # if not lowercase, set all_lower to false and break
             all_lower = False
             break
-# print result (true if all letters are lowercase and at least one letter exists, else false)
+print(f"Result: {all_lower if found_letter else False}") # print result (true if all letters are lowercase and at least one letter exists, else false)
