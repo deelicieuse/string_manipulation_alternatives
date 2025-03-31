cd5b3889acf5386a4babf0c74a868b16fdacecd3
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:09:54 +0800

    input string and prefix

--- manual_startswith.py
+++ manual_startswith.py
@@ -1,4 +1,5 @@
-# prompt for string and prefix
+user_input = input("Enter a string: ") # prompt for string and prefix
+prefix = input("Enter prefix: ")
 #calculate length of prefix
 # compare extracted substring with prefix
 # print true if they match, else print false
