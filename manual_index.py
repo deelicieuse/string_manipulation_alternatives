cc9b47223cca9cd6c1123ab0b32c8e2bb56aef71
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:47:00 +0800

    pseudocode

---
+++ manual_index.py
@@ -0,0 +1,4 @@
+# prompt for string and substring to find
+# loop over each starting index in the string, check for match
+# if a match is found, print index and break
+# if no match is found, print "Not found"
