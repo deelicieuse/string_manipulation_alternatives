7a96798349801890066855baadaf9fcdd04a0ef1
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:28:26 +0800

    if zeros needed is positive, add zeros to string

--- manual_zfill.py
+++ manual_zfill.py
@@ -1,6 +1,7 @@
 user_input = input("Enter a string: ") # prompt for string and width
 width = int(input("Enter total width: "))
 num_zeros = width - len(user_input) # calculate zeros as width minus string length
-# if zeros needed is positive, add zeros to string
+if num_zeros > 0: # if zeros needed is positive,
+    result = "0" * num_zeros + user_input  # add zeros to string
 # else, set string to result
 # print result
