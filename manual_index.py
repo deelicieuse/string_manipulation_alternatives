5f0a49d9d834c1776ac7a110b29455cdf47af14c
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:48:39 +0800

    loop over each index start in the string, check for match

--- manual_index.py
+++ manual_index.py
@@ -2,6 +2,6 @@ user_input = input("Enter a string: ") # prompt for string and substring to find
 sub_str = input("Enter substring to find: ")
 sub_len = len(sub_str)
 found = False
-# loop over each starting index in the string, check for match
+for i in range(len(user_input) - sub_len + 1): # loop over each starting index in the string, check for match
 # if a match is found, print index and break
 # if no match is found, print "Not found"
