1fe82f85e1bf2fd18c92c7e1e750a2d037423987
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 22:55:30 +0800

    pseudocode

---
+++ manual_islower.py
@@ -0,0 +1,5 @@
+# prompt for string
+# set flags
+# for each character, if letter then mark found_letter
+    # if not lowercase, set all_lower to false and break
+# print result (true if all letters are lowercase and at least one letter exists, else false)
