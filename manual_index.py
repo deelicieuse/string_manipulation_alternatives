7d03cfdca50e6b0935a372d06542ec8dcd9855c7
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:47:31 +0800

    input string and substring

--- manual_index.py
+++ manual_index.py
@@ -1,4 +1,5 @@
-# prompt for string and substring to find
+user_input = input("Enter a string: ") # prompt for string and substring to find
+sub_str = input("Enter substring to find: ")
 # loop over each starting index in the string, check for match
 # if a match is found, print index and break
 # if no match is found, print "Not found"
