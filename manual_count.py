13bba9b0c2c62a27302c87dba9aa439a3d2afcfe
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:32:57 +0800

    input string and substring

--- manual_count.py
+++ manual_count.py
@@ -1,4 +1,5 @@
-# prompt for string and substring to count
+user_input = input("Enter a string: ")# prompt for string and substring to count
+sub_str = input("Enter substring to count: ")
 # initialize count to 0
 # loop over each start of string, check if substring matches
 # add count if match found
