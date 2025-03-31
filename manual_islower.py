f404b421e8361b2502c145f77e5061d7a02a1236
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 22:57:49 +0800

    input string

--- manual_islower.py
+++ manual_islower.py
@@ -1,4 +1,4 @@
-# prompt for string
+user_input = input("Enter a string: ") # prompt for string
 # set flags
 # for each character, if letter then mark found_letter
     # if not lowercase, set all_lower to false and break
