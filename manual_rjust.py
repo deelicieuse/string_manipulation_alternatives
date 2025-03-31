5d9237afa095b5710e7f0705a23f9c4878e71c98
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:16:38 +0800

    calculate needed spaces as width minus string length

--- manual_rjust.py
+++ manual_rjust.py
@@ -1,5 +1,5 @@
 user_input = input("Enter a string: ") # prompt for string and width
 width = int(input("Enter total width: "))
-# calculate needed spaces as width minus string length
+num_spaces = width - len(user_input) # calculate needed spaces as width minus string length
 # if spaces needed is positive, set spaces to string
 # print result
