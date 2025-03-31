d94b7e8af7952e010c45a68abff6a0f056928406
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:30:14 +0800

    fixed indentation

--- manual_startswith.py
+++ manual_startswith.py
@@ -3,5 +3,5 @@ prefix = input("Enter prefix: ")
 prefix_len = len(prefix) # calculate length of prefix
 if user_input[:prefix_len] == prefix: # compare extracted substring with prefix
     print("Result: True") # print true if they match
-    else:
+else:
     print("Result: False") # else print false
