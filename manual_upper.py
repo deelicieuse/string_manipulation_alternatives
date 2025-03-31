15809958edd949122e9c748adefdb90154c42829
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 22:50:36 +0800

    else, add character

--- manual_upper.py
+++ manual_upper.py
@@ -3,5 +3,6 @@ result = "" # initialize empty result
 for char in user_input: # for each character,
     if "a" <= char <= "z": # if lowercase, convert to uppercase
         result += chr(ord(char) - 32)
-# else, add character
+    else: # else, add character
+        result += char
 # print result
