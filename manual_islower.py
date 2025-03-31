e7424017aa4aa9f65180d35dd461c925fd65a7ff
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:03:30 +0800

    for each character, if letter then mark found_letter

--- manual_islower.py
+++ manual_islower.py
@@ -1,6 +1,8 @@
 user_input = input("Enter a string: ") # prompt for string
 found_letter = False # set flags
 all_lower = True
-# for each character, if letter then mark found_letter
-    # if not lowercase, set all_lower to false and break
+for char in user_input: # for each character,
+    if ("a" <= char <= "z") or ("A" <= char <= "Z"):     # if letter then mark found_letter
+        found_letter = True
+        # if not lowercase, set all_lower to false and break
 # print result (true if all letters are lowercase and at least one letter exists, else false)
