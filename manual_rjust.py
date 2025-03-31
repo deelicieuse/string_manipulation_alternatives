5dd0f479969bf96bf616c4438bc99627d6f0c906
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:15:40 +0800

    input string and width

--- manual_rjust.py
+++ manual_rjust.py
@@ -1,4 +1,5 @@
-# prompt for string and width
+user_input = input("Enter a string: ") # prompt for string and width
+width = int(input("Enter total width: "))
 # calculate needed spaces as width minus string length
 # if spaces needed is positive, set spaces to string
 # print result
