2d62b298a1dd3913e6b6ef097bda164fb041663c
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:27:24 +0800

    calculate zeros as width minus string length

--- manual_zfill.py
+++ manual_zfill.py
@@ -1,6 +1,6 @@
 user_input = input("Enter a string: ") # prompt for string and width
 width = int(input("Enter total width: "))
-# calculate zeros as width minus string length
+num_zeros = width - len(user_input) # calculate zeros as width minus string length
 # if zeros needed is positive, add zeros to string
 # else, set string to result
 # print result
