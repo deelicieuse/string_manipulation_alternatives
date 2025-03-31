03bb663d5f7c52ac369199bd3480425aeb019e72
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 22:58:20 +0800

    set flags

--- manual_islower.py
+++ manual_islower.py
@@ -1,5 +1,6 @@
 user_input = input("Enter a string: ") # prompt for string
-# set flags
+found_letter = False # set flags
+all_lower = True
 # for each character, if letter then mark found_letter
     # if not lowercase, set all_lower to false and break
 # print result (true if all letters are lowercase and at least one letter exists, else false)
