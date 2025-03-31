db8bc78275d16dad184a2d2947b6f0ccb589392b
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:12:28 +0800

    print true if they match, else print false

--- manual_startswith.py
+++ manual_startswith.py
@@ -2,4 +2,6 @@ user_input = input("Enter a string: ") # prompt for string and prefix
 prefix = input("Enter prefix: ")
 prefix_len = len(prefix) # calculate length of prefix
 if user_input[:prefix_len] == prefix: # compare extracted substring with prefix
-# print true if they match, else print false
+    print("Result: True") # print true if they match
+    else:
+    print("Result: False") # else print false
