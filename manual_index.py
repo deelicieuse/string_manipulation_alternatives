ec1b32bdcef75792f86fd57a3f2db7ee609eaa25
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:47:56 +0800

    calculate length and set flag

--- manual_index.py
+++ manual_index.py
@@ -1,5 +1,7 @@
 user_input = input("Enter a string: ") # prompt for string and substring to find
 sub_str = input("Enter substring to find: ")
+sub_len = len(sub_str)
+found = False
 # loop over each starting index in the string, check for match
 # if a match is found, print index and break
 # if no match is found, print "Not found"
