02bf4861c89d7e2c4166410cd3d9337c10d205d4
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:29:00 +0800

    else, set string to result

--- manual_zfill.py
+++ manual_zfill.py
@@ -3,5 +3,6 @@ width = int(input("Enter total width: "))
 num_zeros = width - len(user_input) # calculate zeros as width minus string length
 if num_zeros > 0: # if zeros needed is positive,
     result = "0" * num_zeros + user_input  # add zeros to string
-# else, set string to result
+else: # else, set string to result
+    result = user_input
 # print result
