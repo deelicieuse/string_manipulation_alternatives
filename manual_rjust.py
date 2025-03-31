17d0229062f6a18b50424a1707be894e4b42bde3
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:21:34 +0800

    # if spaces needed is positive, add spaces to string

--- manual_rjust.py
+++ manual_rjust.py
@@ -1,5 +1,8 @@
 user_input = input("Enter a string: ") # prompt for string and width
 width = int(input("Enter total width: "))
 num_spaces = width - len(user_input) # calculate needed spaces as width minus string length
-# if spaces needed is positive, set spaces to string
+if num_spaces > 0: # if spaces needed is positive, add spaces to string
+    result = " " * num_spaces + user_input
+else:
+    result = user_input
 # print result
