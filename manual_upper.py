843ca606ab5f3fbef61e2555a258ccc3ae0a80cb
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 22:47:54 +0800

    input string

--- manual_upper.py
+++ manual_upper.py
@@ -1,4 +1,4 @@
-# prompt for string
+user_input = input("Enter a string: ") # prompt for string
 # initialize empty result
 # for each character, if lowercase, convert to uppercase w/ ASCII math
 # else, add character
