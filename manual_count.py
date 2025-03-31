b2e02560137cb41ce051c6ff9697336707a5957a
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:35:26 +0800

    print result

--- manual_count.py
+++ manual_count.py
@@ -5,4 +5,4 @@ sub_len = len(sub_str)
 for i in range(len(user_input) - sub_len + 1): # loop over each start of string, check if substring matches
     if user_input[i:i + sub_len] == sub_str:
         count += 1 # add count if match found
-# print count
+print(f"Result: {result}") # print count
