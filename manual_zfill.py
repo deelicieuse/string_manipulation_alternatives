95a13d43b38089180f980274821e34e39da6b5a5
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:26:46 +0800

    input string and width

--- manual_zfill.py
+++ manual_zfill.py
@@ -1,4 +1,5 @@
-# prompt for string and width
+user_input = input("Enter a string: ") # prompt for string and width
+width = int(input("Enter total width: "))
 # calculate zeros as width minus string length
 # if zeros needed is positive, add zeros to string
 # else, set string to result
