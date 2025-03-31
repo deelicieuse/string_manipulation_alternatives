2a2e938dae6300a18df9a63811a754bb88b76e2e
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:10:23 +0800

    calculate length of prefix

--- manual_startswith.py
+++ manual_startswith.py
@@ -1,5 +1,5 @@
 user_input = input("Enter a string: ") # prompt for string and prefix
 prefix = input("Enter prefix: ")
-#calculate length of prefix
+prefix_len = len(prefix) # calculate length of prefix
 # compare extracted substring with prefix
 # print true if they match, else print false
