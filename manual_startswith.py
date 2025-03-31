4bafac3b31445c40bbe9cd3e7bc5987047df1f11
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:11:26 +0800

    compare extracted substring with prefix

--- manual_startswith.py
+++ manual_startswith.py
@@ -1,5 +1,5 @@
 user_input = input("Enter a string: ") # prompt for string and prefix
 prefix = input("Enter prefix: ")
 prefix_len = len(prefix) # calculate length of prefix
-# compare extracted substring with prefix
+if user_input[:prefix_len] == prefix: # compare extracted substring with prefix
 # print true if they match, else print false
