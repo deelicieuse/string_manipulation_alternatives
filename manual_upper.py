4a17f7efaf559ebadfd3b889bd595f25e1efad47
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 22:48:29 +0800

    initialize empty result

--- manual_upper.py
+++ manual_upper.py
@@ -1,5 +1,5 @@
 user_input = input("Enter a string: ") # prompt for string
-# initialize empty result
+result = "" # initialize empty result
 # for each character, if lowercase, convert to uppercase w/ ASCII math
 # else, add character
 # print result
