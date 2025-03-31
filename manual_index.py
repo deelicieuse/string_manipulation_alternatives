30d2ac03fe637d0e9c81761c651fa32d494a12b1
deelicieuse <libatiqueangelique@gmail.com>
2025-03-31 23:41:07 +0800

    if  match found, print index and break

--- manual_index.py
+++ manual_index.py
@@ -3,5 +3,8 @@ sub_str = input("Enter substring to find from the right: ")
 sub_len = len(sub_str)
 found = False
 for i in range(len(user_input) - sub_len, -1, -1): # loop over each index start in the string, check for a match
-# if  match found, print index and break
+    if user_input[i:i + sub_len] == sub_str: # if  match found, print index and break
+        print("Result:", i)
+        found = True
+        break
 # else, print "Not Found"
